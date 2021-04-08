273060a1ae851e91ad9d8f64a0d4e73968da51a7
Mengyi Zhou <zmengyi@amazon.com>
2021-04-08 12:05:34 -0700

    add container insight tests

--- terraform/containerinsight_eks_prometheus/main.tf
+++ terraform/containerinsight_eks_prometheus/main.tf
@@ -77,6 +77,7 @@ provider "helm" {
   }
 }
 
+
 # create a unique namespace for each run
 resource "kubernetes_namespace" "aoc_ns" {
   metadata {
@@ -107,6 +108,168 @@ resource "kubernetes_cluster_role_binding" "aoc_role_binding" {
     namespace = kubernetes_namespace.aoc_ns.metadata[0].name
   }
 }
+
+resource "kubernetes_config_map" "aoc_configmap" {
+  metadata {
+    name      = "otel-agent-conf"
+    namespace = kubernetes_namespace.aoc_ns.metadata[0].name
+  }
+  data = {
+    "otel-agent-config" = "${file("${var.testcase}/otel-agent-config.yaml")}"
+  }
+}
+
+resource "kubernetes_daemonset" "aoc_ds" {
+  metadata {
+    name      = "aoc"
+    namespace = kubernetes_namespace.aoc_ns.metadata[0].name
+    labels = {
+      app = "aoc"
+    }
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        app = "aoc"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "aoc"
+        }
+      }
+
+      spec {
+        service_account_name            = "aoc-role-${module.common.testing_id}"
+        automount_service_account_token = true
+        # aoc
+        container {
+          name  = "aoc"
+          image = module.common.aoc_image
+          command = [
+            "/awscollector",
+          "--config=/conf/otel-agent-config.yaml"]
+          image_pull_policy = "Always"
+          env {
+            name  = "AWS_REGION"
+            value = var.region
+          }
+          env {
+            name = "K8S_NODE_NAME"
+            value_from {
+              field_ref {
+                field_path = "spec.nodeName"
+              }
+            }
+          }
+          env {
+            name = "HOST_IP"
+            value_from {
+              field_ref {
+                field_path = "status.hostIP"
+              }
+            }
+          }
+          env {
+            name = "HOST_NAME"
+            value_from {
+              field_ref {
+                field_path = "spec.nodeName"
+              }
+            }
+          }
+          env {
+            name = "K8S_NAMESPACE"
+            value_from {
+              field_ref {
+                field_path = "metadata.namespace"
+              }
+            }
+          }
+          resources {
+            requests {
+              cpu    = "0.2"
+              memory = "256Mi"
+            }
+          }
+          volume_mount {
+            name       = "rootfs"
+            mount_path = "/rootfs"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "dockersock"
+            mount_path = "/var/run/docker.sock"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "varlibdocker"
+            mount_path = "/var/lib/docker"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "sys"
+            mount_path = "/sys"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "devdisk"
+            mount_path = "/dev/disk"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "otel-agent-config-vol"
+            mount_path = "/conf"
+          }
+        }
+        volume {
+          name = "otel-agent-config-vol"
+          config_map {
+            name = "otel-agent-conf"
+            items {
+              key  = "otel-agent-config"
+              path = "otel-agent-config.yaml"
+            }
+          }
+        }
+        volume {
+          name = "rootfs"
+          host_path {
+            path = "/"
+          }
+        }
+        volume {
+          name = "dockersock"
+          host_path {
+            path = "/var/run/docker.sock"
+          }
+        }
+        volume {
+          name = "varlibdocker"
+          host_path {
+            path = "/var/lib/docker"
+          }
+        }
+        volume {
+          name = "sys"
+          host_path {
+            path = "/sys"
+          }
+        }
+        volume {
+          name = "devdisk"
+          host_path {
+            path = "/dev/disk/"
+          }
+        }
+      }
+    }
+  }
+}
+
 resource "kubernetes_deployment" "standalone_aoc_deployment" {
   metadata {
     name      = "aoc"
@@ -163,49 +326,6 @@ resource "kubernetes_deployment" "standalone_aoc_deployment" {
   }
 }
 
-module "demo_nginx" {
-  source = "./nginx"
-
-  kubeconfig = local_file.kubeconfig.filename
-  testcase   = var.testcase
-  testing_id = module.common.testing_id
-}
-
-module "demo_appmesh" {
-  source = "./appmesh"
-
-  kubeconfig            = local_file.kubeconfig.filename
-  provider_url          = data.aws_eks_cluster.testing_cluster.identity[0].oidc[0].issuer
-  region                = var.region
-  sample_app_image_repo = var.sample_app_image_repo
-  testcase              = var.testcase
-  testing_id            = module.common.testing_id
-}
-
-module "demo_jmx" {
-  source = "./jmx"
-
-  sample_app_image_repo = var.sample_app_image_repo
-  testcase              = var.testcase
-  testing_id            = module.common.testing_id
-}
-
-module "demo_memcached" {
-  // source folder name cannot be the same as the chart name: https://github.com/hashicorp/terraform-provider-helm/issues/509
-  source = "./memcached-service"
-
-  testcase   = var.testcase
-  testing_id = module.common.testing_id
-}
-
-module "demo_haproxy" {
-  // source folder name cannot be the same as the chart name: https://github.com/hashicorp/terraform-provider-helm/issues/509
-  source = "./haproxy"
-
-  testcase   = var.testcase
-  testing_id = module.common.testing_id
-}
-
 ##########################################
 # Validation
 ##########################################
@@ -216,29 +336,6 @@ module "validator" {
   region            = var.region
   testing_id        = module.common.testing_id
   metric_namespace  = "${module.common.otel_service_namespace}/${module.common.otel_service_name}"
-  cloudwatch_context_json = jsonencode({
-    clusterName : var.eks_cluster_name
-    appMesh : {
-      namespace : module.demo_appmesh.metric_dimension_namespace
-      job : "kubernetes-pod-appmesh-envoy"
-    }
-    nginx : {
-      namespace : module.demo_nginx.metric_dimension_namespace
-      job : "kubernetes-service-endpoints"
-    }
-    jmx : {
-      namespace : module.demo_jmx.metric_dimension_namespace
-      job : "kubernetes-pod-jmx"
-    }
-    memcached : {
-      namespace : module.demo_memcached.metric_dimension_namespace
-      job : "kubernetes-service-endpoints"
-    }
-    haproxy : {
-      namespace : module.demo_haproxy.metric_dimension_namespace
-      job : "kubernetes-service-endpoints"
-    }
-  })
 
   aws_access_key_id     = var.aws_access_key_id
   aws_secret_access_key = var.aws_secret_access_key
